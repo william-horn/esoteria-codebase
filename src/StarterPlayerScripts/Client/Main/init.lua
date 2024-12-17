80960dd7227623df218ad68069364fe4e2136242
William Horn <williamjosephhorn@gmail.com>
2024-12-17 15:52:30 -0500

    STABLE new Network arcitecture with working server-to-client and client-to-server examples

--- src/StarterPlayerScripts/Client/Main/init.lua
+++ src/StarterPlayerScripts/Client/Main/init.lua
@@ -1,8 +1,20 @@
 
 return function(import, _, pm)
 	print("started server")
-	pm.from(script).import("Network")
-	
+	local enums = game.ReplicatedStorage.Enums.Network
+	local network = pm.from(script).import("Network")
 
+	network:init()
+	
+	network:sendTCP(
+		{
+			channel = enums.Channel.Player,
+			request = enums.Request.GetPlayer,
+			_dependencies = {"same"},
+		},
+		{
+			data = "FROM CLIENT"
+		}
+	)
 end
 
