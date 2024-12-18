4147881f482f6176bd0b37149c2f1f3c00a38045
William Horn <williamjosephhorn@gmail.com>
2024-12-18 00:22:40 -0500

    STABLE - working CustomCamera

--- src/StarterPlayerScripts/Client/Main/init.lua
+++ src/StarterPlayerScripts/Client/Main/init.lua
@@ -3,5 +3,14 @@ return function(import, _, pm)
 	local packages = pm.from(script.Dependencies)
 
 	local Network = packages.import("Network"):init()
+	local UserInputHandler = packages.import("UserInputHandler"):init()
+	local CustomCamera = packages.import("CustomCamera"):init()
+
+	CustomCamera:setMode(game.ReplicatedStorage.Enums.CameraMode.TrackPlayerNormal)
+	
+	local c = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
+
+	CustomCamera:setTarget(c.HumanoidRootPart)
+
 end
 
