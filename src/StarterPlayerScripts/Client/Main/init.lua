15a1e2f74ffffbe097dd3998ee2e7cb443ccc304
William Horn <williamjosephhorn@gmail.com>
2024-12-17 23:34:29 -0500

    STABLE - working PlayerData retrieval with new Network module

--- src/StarterPlayerScripts/Client/Main/init.lua
+++ src/StarterPlayerScripts/Client/Main/init.lua
@@ -1,20 +1,7 @@
 
 return function(import, _, pm)
-	local enums = game.ReplicatedStorage.Enums.Network
-	local network = pm.from(script).import("Network*")
+	local packages = pm.from(script.Dependencies)
 
-	network:init()
-	
-	network:sendTCP(
-		{
-			channel = enums.Channel.Common,
-			request = enums.Request.GetPlayer,
-			_dependencies = {"same"},
-		},
-		{
-			data = "FROM CLIENT"
-		}
-	)
-
+	local Network = packages.import("Network"):init()
 end
 
